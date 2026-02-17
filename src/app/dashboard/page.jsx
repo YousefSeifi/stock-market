fa71700c3a35aacac76e4b27f1d8ab3cee95ee87
Yousef seifi <yoosefseifi.84@gmail.com>
2026-02-17 23:24:30 +0330

    add dashboard file

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+// app/dashboard/page.jsx
+import React from "react";
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen p-4">
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+    </div>
+  );
+}
